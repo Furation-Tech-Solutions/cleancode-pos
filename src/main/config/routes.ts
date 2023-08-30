fad9235aeeca343fead152c86bd628bbd175bd9a
Survesh Pandit <survesh.pandit@furation.tech>
2023-08-30 13:50:51 +0530

    feat: completed the foodMenu Schema

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -9,17 +9,12 @@ import { ingredientRouter } from "@presentation/routes/ingredient-routes";
 import { deliveryPartnerRouter } from "@presentation/routes/deliveryPartner-routes";
 import { foodCategoryRouter } from "@presentation/routes/foodCategory-routes";
 import { kitchenRouter } from "@presentation/routes/kitchen-routes";
-import { ingredientRouter } from "@presentation/routes/ingredient-routes";
-import { deliveryPartnerRouter } from "@presentation/routes/deliveryPartner-routes";
-import { foodCategoryRouter } from "@presentation/routes/foodCategory-routes";
-import { kitchenRouter } from "@presentation/routes/kitchen-routes";
 import {outletRouter } from "@presentation/routes/outlet-route";
 import { type Express, Router } from "express";
 import ApiError from "@presentation/error-handling/api-error";
 import { companyRouter } from "@presentation/routes/company-route";
 import { cuisineRouter } from "@presentation/routes/cuisine-routes";
 import { foodMenuRouter } from "@presentation/routes/foodMenu-routes";
-import { cuisineRouter } from "@presentation/routes/cuisine-routes";
 
 export default (app: Express): void => {
   const router = Router();
@@ -37,12 +32,12 @@ export default (app: Express): void => {
   app.use("/ingredientCategory", ingredientCategoryRouter);
   app.use("/ingredient", ingredientRouter);
   app.use("/deliveryPartner", deliveryPartnerRouter);
-  app.use("/ingredient", ingredientRouter);
-  app.use("/deliveryPartner", deliveryPartnerRouter);
   app.use("/admin",adminRouter);
   app.use("/outlet", outletRouter);
   app.use("/company", companyRouter);
   app.use(router);
   app.use("/staff", staffRouter);
   app.use("/inventory", inventoryRouter);
+  app.use("/cuisine", cuisineRouter);
+  app.use("/foodMenu", foodMenuRouter);
 };
